599920f98a84da1d77abb652ee914a6e51a9ca11
nghia7217 <nghia7217@gmail.com>
2022-01-07 15:52:02 +0700

    Hoan_chinh_trang_home

--- src/components/familypackage/Familypackage.jsx
+++ src/components/familypackage/Familypackage.jsx
@@ -1,9 +1,28 @@
 import "./familypackage.css"
-
+import "../chart/chart.css"
+import{CalendarOutlined } from '@ant-design/icons'
+import chart from "./img/chartfamily.png"
 export default function Familypackage() {
     return (
         <div className="familypackage">
-            bbbbbbb
+            <div className="familypackage__left">         
+                <ul className="chart__name-header"> 
+                    <li className="chart__name-right">
+                      <span className="chart__name-right-day">
+                        Tháng 4, 2021
+                      </span>
+                        <CalendarOutlined className="chart__name-right-item"/>
+                    </li>
+                </ul>
+           
+            </div>
+            <div className="familypackage__right">
+                <h3 className="familypackage__right-title">
+                    Gói gia đình
+                </h3>
+                <img src={chart} alt="" className="" className="familypackage__right-img"/>
+            </div>
+            
         </div>
     )
 }
--- src/components/ticketeven/Ticketeven.jsx
+++ src/components/ticketeven/Ticketeven.jsx
@@ -1,9 +1,29 @@
 import "./ticketeven.css"
-
+import rec from "./img_ticket/Rectangle.png"
+import rec1 from "./img_ticket/Rectangle2.png"
+import charteven from "./img_ticket/goisukien.png"
 export default function Ticketeven() {
     return (
         <div className="ticketeven">
-            aaaaa
-        </div>
+            <div className="ticketeven__left">
+                <h3 className="ticketeven__left-title">
+                    Gói sự kiện
+                </h3>
+                <img src={charteven} alt="" className="" className="ticketeven__left-img"/>
+            </div>
+            <div className="ticketeven__right">
+                <ul className="ticketeven__right-ul">
+                    <li>
+                        <img src={rec1} alt="" className="ticketeven__right-img"/>             
+                        Vé đã sử dụng
+                    </li>
+                    <li>
+                        <img src={rec} alt="" className="ticketeven__right-img"/>  
+                        Vé chưa sử dụng
+                    </li>
+                </ul>
+            </div>
+
+        </div>    
     )
 }
