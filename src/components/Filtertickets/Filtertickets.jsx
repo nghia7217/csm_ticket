86bc9cd59826ccb40dd687f333fea144c0acb8e7
nghia7217 <nghia7217@gmail.com>
2022-01-12 21:19:49 +0700

    Desight_layout

--- src/components/Filtertickets/Filtertickets.jsx
+++ src/components/Filtertickets/Filtertickets.jsx
@@ -13,18 +13,32 @@ export default function Filtertickets() {
     const [value, setValue] = React.useState(null);
     return (
         <div className="filtertickets">
-            <div>Lọc vé</div>
-            <ul>
-                <li>Tình trạng đối soát</li>
-                <li>Loại vé</li>
-                <li>Từ ngày</li>
-                <li>Đến ngày</li>
-            </ul>
-
-
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-                 <Stack spacing={3}>
-                 <DatePicker
+            <div className="filtertickets__header"> Lọc vé</div>
+            <div>
+                <ul className="filtertickets__ul">
+                    <li className="filtertickets__li filtertickets__li-title">Tình trạng đối soát</li>
+                    <div className="test">
+                        <li className="filtertickets__li">
+                            <input type="radio" className="filtertickets__li-input" /> Tất cả
+                        </li>
+                        <li className="filtertickets__li">
+                            <input type="radio" className="filtertickets__li-input" /> Đã đối soát
+                        </li>
+                        <li className="filtertickets__li">
+                            <input type="radio"  className="filtertickets__li-input"/> Chưa đối soát
+                        </li>
+                    </div>
+                </ul>
+                <ul className="filtertickets__ul">
+                    <li className="filtertickets__li filtertickets__li-title ">Loại vé</li>
+                    <li className="filtertickets__li filtertickets__li-margin">Vé cổng</li>
+                </ul>
+                <ul className="filtertickets__ul">
+                    <li className="filtertickets__li filtertickets__li-title">Từ ngày</li>
+                    <li className="filtertickets__li">
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                         <Stack spacing={3}>
+                            <DatePicker
                     label="12/1/2022"
                     disabled
                     value={value}
@@ -34,7 +48,30 @@ export default function Filtertickets() {
                     renderInput={(params) => <TextField {...params} />}
                  />
                 </Stack>
-            </LocalizationProvider>    
+                    </LocalizationProvider>  
+                    </li>
+                </ul>
+                <ul className="filtertickets__ul">
+                    <li className="filtertickets__li filtertickets__li-title">Đến ngày</li>
+                    <li className="filtertickets__li">
+                            <LocalizationProvider dateAdapter={AdapterDateFns} >
+                                <DatePicker 
+                                    label="mm/dd/yy"
+                                    value={value}
+                                    onChange={(newValue) => {
+                                    setValue(newValue);
+                                    }}
+                                    renderInput={(params) => <TextField {...params} />}
+                                />
+                            </LocalizationProvider>
+                    </li>
+                </ul>
+            </div>
+           
+                    
+            
+             
+
         </div>
         
     )
