02cb90293b86385e1d28c64b762c0717bc988277
nghia7217 <nghia7217@gmail.com>
2022-01-18 21:03:15 +0700

    modal_ticketlist

--- src/pages/ticketlist/Ticketlist.jsx
+++ src/pages/ticketlist/Ticketlist.jsx
@@ -3,11 +3,15 @@ import Header from "../../components/Header/Header";
 import Pagination from "../../components/Pagination/Pagination";
 import TableContent from "../../components/Tablecontent/TableContent";
 import Headerticket from "../../components/Headerticket/Headerticket";
+
+
 export default function Ticketlist() {
-     
+ 
+
   
     return (
         <div className="tickerlist">
+          
           <Header name="Danh sách vé"/>
           <Headerticket namebtnone="Lọc vé" namebtntwo="Xuất file (.csv)"/>
           <TableContent row1="STT" row2="Booking code" row3="Số vé" row4="Tình trạng sử dụng" row5="Ngày sử dụng" row6="Ngày xuất vé" row7="Cổng check - in"/>
