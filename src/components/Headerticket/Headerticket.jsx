194570b9e58ba3a390d0c74eeaf7cae9a2e947cb
nghia7217 <nghia7217@gmail.com>
2022-01-24 13:41:58 +0700

    desight_filterTicket

--- src/components/Headerticket/Headerticket.jsx
+++ src/components/Headerticket/Headerticket.jsx
@@ -4,10 +4,8 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
+import Modalfilter from "../Modal/Modalfilter/Modalfilter";
+
 const style = {
     position: 'absolute',
     top: '50%',
@@ -17,12 +15,12 @@ const style = {
     border: '2px solid #fff',
     p: 4,
     borderRadius: '10px',
+    width: 400,
   };
 export default function Headerticket(props) {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [value, setValue] = React.useState(null);
   
     return (
         <div className="headerticket">
@@ -33,7 +31,6 @@ export default function Headerticket(props) {
                         <SearchOutlined className="headerticket__search-icon"/>
                     </li>
                 </ul>
-
                 <ul className="headerticket__list">
                     <Button onClick={handleOpen}>      
                                 <FilterOutlined className="headerticket__list-icon"/>
@@ -41,85 +38,7 @@ export default function Headerticket(props) {
                     </Button>
                     <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
                         <Box sx={style}>
-                            <h3 className="box__header">Lọc vé</h3>
-                            <ul className="box__content">
-                                <li className="box__li"> 
-                                    <h5>Từ ngày </h5>
-                                    <br></br>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <DatePicker
-                                        label="18/1/2022"
-                                        value={value}
-                                        onChange={(newValue) => {
-                                        setValue(newValue);
-                                        }}
-                                        renderInput={(params) => <TextField {...params} />}
-                                    />
-                                </LocalizationProvider>
-                                </li>
-                                <li className="box__li">
-                                <h5>Đến ngày</h5>   
-                                <br></br>
-                                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                                    <DatePicker
-                                        label="18/1/2022"
-                                        value={value}
-                                        onChange={(newValue) => {
-                                        setValue(newValue);
-                                        }}
-                                        renderInput={(params) => <TextField {...params} />}
-                                    />
-                                </LocalizationProvider>
-                                </li>
-                            </ul>
-                            <h5>Tình trạng sử dụng</h5>
-                            <ul className="box__content">
-                                <li className="box__li box__list">
-                                    <input type="radio" />
-                                    Tất cả
-                                </li>
-                                <li className="box__li box__list">
-                                    <input type="radio" />
-                                    Đã sử dụng
-                                </li>
-                                <li className="box__li box__list">
-                                    <input type="radio" />
-                                    Chưa sử dụng
-                                </li>
-                                <li className="box__li box__list">
-                                    <input type="radio" />
-                                    Hết hạn
-                                </li>
-                            </ul>
-                            <h5>Cổng Check - in</h5>
-                            <ul className="box__content">
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Tất cả
-                                </li>
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Cổng 1
-                                </li>
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Cổng 2
-                                </li>
-                                <br></br>
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Cổng 3
-                                </li> 
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Cổng 4
-                                </li>
-                                <li className="box__list box__li">
-                                    <input type="checkbox" />
-                                    Cổng 5
-                                </li>
-                            </ul>
-                            <button type="button" className="filtertickets__btn">Lọc</button>
+                            <Modalfilter/>
                         </Box>
                     </Modal>
                     <li className="headerticket__list-item headerticket__list-item-button">
